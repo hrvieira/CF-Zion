001b1ea6b79d45afbbb9a8e7bfa9126e103e84d3
hrvieira <luizhrvieira@outlook.com>
2023-11-17 01:48:45 -0300

    Primeiro projeto real e independente.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Seleciona o elemento com a classe "titulo"
+var tituloElement = document.querySelector('.titulo');
+
+// Converte o texto do elemento para maiúsculas
+tituloElement.textContent = tituloElement.textContent.toUpperCase();
